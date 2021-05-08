367c25be3552d770c71ae579d9b010b9af039db5
NjohPrince <jufredprince@gmail.com>
2021-05-08 11:24:03 +0100

    project 2 goldBagChecker

---
+++ JavaPrograms_Assignment/JavaPrograms_Assignment/GoldbagChecker.java
@@ -0,0 +1,23 @@
+package JavaPrograms_Assignment;
+
+import java.util.Scanner;
+
+/**********
+ * GoldBag Checker *********
+ * 
+ * @param author *
+ * @return "Njoh Noh Prince Junior" *
+ *********************************************/
+
+public class GoldbagChecker {
+    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Enter a positive number: ");
+        number = scanner.nextInt();
+        
+        scanner.close();
+    }
+}
