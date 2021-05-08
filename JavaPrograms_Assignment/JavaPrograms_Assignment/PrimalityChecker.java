1bca1d52f53b22cba5e6c568dc5452572e18b8ce
NjohPrince <jufredprince@gmail.com>
2021-05-08 11:21:34 +0100

    created a static function returning if a number is prime or not

--- JavaPrograms_Assignment/JavaPrograms_Assignment/PrimalityChecker.java
+++ JavaPrograms_Assignment/JavaPrograms_Assignment/PrimalityChecker.java
@@ -10,7 +10,14 @@ import java.util.Scanner;
  *********************************************/
 
 public class PrimalityChecker {
-    
+    public static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,7 +26,11 @@ public class PrimalityChecker {
             System.out.println("Enter a positive number: ");
             number = scanner.nextInt();
         } while (number < 0);
-        
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime number");
+        } else {
+            System.out.println(number + " is not a Prime number");
+        }
         scanner.close();
     }
 }
